9b4a7cef14e6ef0cacd13962362497cbb515042d
Enzo Rudy SEKKAI <enzorudy.sekkai@outlook.com>
2023-02-11 10:38:43 +0100

    fonctionne

--- calibration.py
+++ calibration.py
@@ -5,12 +5,12 @@ import glob
 objpoints = []
 imgpoints = []
 
-chessboardSize = (8, 5)
+chessboardSize = (9, 6)
 
 criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
-objp = np.zeros((5 * 8, 3), np.float32)
-objp[:, :2] = np.mgrid[0:7, 0:6].T.reshape(-1, 2)
+objp = np.zeros((6 * 9, 3), np.float32)
+objp[:, :2] = np.mgrid[0:9, 0:6].T.reshape(-1, 2)
 
 images = glob.glob('*.jpg')
 framesize = (0, 0)
@@ -18,8 +18,8 @@ framesize = (0, 0)
 for fname in images:
     img = cv.imread(fname)
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-    framesize = gray.shape[::-1]
     ret, corners = cv.findChessboardCorners(gray, chessboardSize, None)
+    print(ret)
     if ret:
         objpoints.append(objp)
         corners2 = cv.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
@@ -28,5 +28,5 @@ for fname in images:
         cv.imshow('img', img)
         cv.waitKey(500)
 
-ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, framesize, None, None)
+ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
 np.savez('calibration.npz', mtx=mtx, dist=dist, rvecs=rvecs, tvecs=tvecs)
