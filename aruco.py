44f2976a577c1f76295e97a3e6dabaf13eb10ee7
MarXx33 <mouriche@hotmail.com>
2023-03-02 15:34:20 +0100

    Tweaks

--- aruco.py
+++ aruco.py
@@ -1,128 +1,130 @@
 import numpy as np
 import cv2 as cv
 
-# aruco detection
-dictionary = cv.aruco.getPredefinedDictionary(cv.aruco.DICT_4X4_250)
-parameters = cv.aruco.DetectorParameters()
-detector = cv.aruco.ArucoDetector(dictionary, parameters)
 
-# Variable pour le déplacement
-plus = True
-plusz = False
-
-maxx = 0.2
-minx = -0.2
-
-maxy = 0.2
-miny = -0.2
-
-maxz = -0.1
-minz = -0.05
-
-axisx = 0
-axisy = 0
-axisz = -0.1
-
-# camera calibration
-with np.load('../calibration/calibration.npz') as X:
-    mtx, dist, _, _ = [X[i] for i in ('mtx', 'dist', 'rvecs', 'tvecs')]
+def get_translation_matrix(tvec):
+    t = np.identity(n=4)
+    t[0:3, 3] = tvec
+    return t
 
 
-def getTranslationMatrix(tvec):
-    T = np.identity(n=4)
-    T[0:3, 3] = tvec
-    return T
-
-
-def getTransformMatrix(rvec, tvec):
-    mat = getTranslationMatrix(tvec)
+def get_transform_matrix(rvec, tvec):
+    mat = get_translation_matrix(tvec)
     mat[:3, :3] = cv.Rodrigues(rvec)[0]
     return mat
 
 
-def relativeTransformMatrix(rotation, translation):
-    xC, xS = np.cos(rotation[0]), np.sin(rotation[0])
-    yC, yS = np.cos(rotation[1]), np.sin(rotation[1])
-    zC, zS = np.cos(rotation[2]), np.sin(rotation[2])
-    dX = translation[0]
-    dY = translation[1]
-    dZ = translation[2]
-    Translate_matrix = np.array([[1, 0, 0, dX],
-                                 [0, 1, 0, dY],
-                                 [0, 0, 1, dZ],
+def relative_transform_matrix(rotation, translation):
+    x_c, x_s = np.cos(rotation[0]), np.sin(rotation[0])
+    y_c, y_s = np.cos(rotation[1]), np.sin(rotation[1])
+    z_c, z_s = np.cos(rotation[2]), np.sin(rotation[2])
+    d_x = translation[0]
+    d_y = translation[1]
+    d_z = translation[2]
+    translate_matrix = np.array([[1, 0, 0, d_x],
+                                 [0, 1, 0, d_y],
+                                 [0, 0, 1, d_z],
                                  [0, 0, 0, 1]])
-    Rotate_X_matrix = np.array([[1, 0, 0, 0],
-                                [0, xC, -xS, 0],
-                                [0, xS, xC, 0],
+    rotate_x_matrix = np.array([[1, 0, 0, 0],
+                                [0, x_c, -x_s, 0],
+                                [0, x_s, x_c, 0],
                                 [0, 0, 0, 1]])
-    Rotate_Y_matrix = np.array([[yC, 0, yS, 0],
+    rotate_y_matrix = np.array([[y_c, 0, y_s, 0],
                                 [0, 1, 0, 0],
-                                [-yS, 0, yC, 0],
+                                [-y_s, 0, y_c, 0],
                                 [0, 0, 0, 1]])
-    Rotate_Z_matrix = np.array([[zC, -zS, 0, 0],
-                                [zS, zC, 0, 0],
+    rotate_z_matrix = np.array([[z_c, -z_s, 0, 0],
+                                [z_s, z_c, 0, 0],
                                 [0, 0, 1, 0],
                                 [0, 0, 0, 1]])
-    return np.dot(Rotate_Z_matrix, np.dot(Rotate_Y_matrix, np.dot(Rotate_X_matrix, Translate_matrix)))
+    return np.dot(rotate_z_matrix, np.dot(rotate_y_matrix, np.dot(rotate_x_matrix, translate_matrix)))
 
 
-axis = np.float32([[3, 0, 0], [0, 3, 0], [0, 0, -3]]).reshape(-1, 3)
+if __name__ == "__main__":
 
-cap = cv.VideoCapture(0)
-
-while True:
-    _, frame = cap.read()
-
-    # frame = cv.resize(frame, None, fx=0.7, fy=0.7, interpolation=cv.INTER_AREA)
-    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
     # aruco detection
-    corners, ids, rejectedImgPoints = detector.detectMarkers(gray)
-    if np.all(ids != None):
-        rvec, tvec, _ = cv.aruco.estimatePoseSingleMarkers(corners, 0.1, mtx, dist)
+    dictionary = cv.aruco.getPredefinedDictionary(cv.aruco.DICT_4X4_250)
+    parameters = cv.aruco.DetectorParameters()
+    detector = cv.aruco.ArucoDetector(dictionary, parameters)
 
-        rvec = rvec[0][0]
-        tvec = tvec[0][0]
+    # Variable pour le déplacement
+    plus = True
+    plus_z = False
 
-        # Get the original marker position in 4x4 matrix representation
-        transformMatrix = getTransformMatrix(rvec, tvec)
+    max_x = 0.2
+    min_x = -0.2
 
-        # Get the transform matrix we want to apply to the obtained marker position
-        # E.g. rotate 180 deg (PI) along Y and then translate -10 cm along X
-        relativeTransformMatrice = relativeTransformMatrix([0, np.pi, 0], [axisx, axisy, axisz])
+    max_y = 0.2
+    min_y = -0.2
 
-        # Now apply the transform to the original matrix by simply dot multiplying them
-        transformMatrix = np.dot(transformMatrix, relativeTransformMatrice)
+    max_z = -0.1
+    min_z = -0.05
 
-        # Extract rotation matrix and translation vector out of result and then display
-        rmat_relative = transformMatrix[:3, :3]
-        tmat_relative = transformMatrix[:3, 3:]
+    axis_x = 0
+    axis_y = 0
+    axis_z = -0.1
 
-        cv.drawFrameAxes(frame, mtx, dist, rmat_relative, tmat_relative, 0.1)
+    # camera calibration
+    with np.load('../calibration/calibration.npz') as X:
+        mtx, dist, _, _ = [X[i] for i in ('mtx', 'dist', 'rvecs', 'tvecs')]
 
-    # Déplacement en y
-    if axisy >= maxy and plus:
-        plus = False
-    if axisy <= miny and not plus:
-        plus = True
-    if plus:
-        axisy += 0.01
-    else:
-        axisy -= 0.01
+    axis = np.float32([[3, 0, 0], [0, 3, 0], [0, 0, -3]]).reshape(-1, 3)
 
-    # Déplacement en Z
-    if axisz >= minz and not plusz:
-        plusz = True
-    if axisz <= maxz and plusz:
-        plusz = False
+    cap = cv.VideoCapture(0)
 
-    if plusz:
-        axisz -= 0.0025
-    else:
-        axisz += 0.0025
+    while True:
+        _, frame = cap.read()
 
-    cv.imshow('frame', frame)
-    if cv.waitKey(1) & 0xFF == ord('q'):
-        break
+        # frame = cv.resize(frame, None, fx=0.7, fy=0.7, interpolation=cv.INTER_AREA)
+        gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+        # aruco detection
+        corners, ids, rejectedImgPoints = detector.detectMarkers(gray)
+        if np.all(ids is not None):
+            rvec, tvec, _ = cv.aruco.estimatePoseSingleMarkers(corners, 0.1, mtx, dist)
 
-cap.release()
-cv.destroyAllWindows()
+            rvec = rvec[0][0]
+            tvec = tvec[0][0]
+
+            # Get the original marker position in 4x4 matrix representation
+            transform_matrix = get_transform_matrix(rvec, tvec)
+
+            # Get the transform matrix we want to apply to the obtained marker position
+            # E.g. rotate 180 deg (PI) along Y and then translate -10 cm along X
+            relative_transform_matrix_ = relative_transform_matrix([0, np.pi, 0], [axis_x, axis_y, axis_z])
+
+            # Now apply the transform to the original matrix by simply dot multiplying them
+            transform_matrix = np.dot(transform_matrix, relative_transform_matrix_)
+
+            # Extract rotation matrix and translation vector out of result and then display
+            rmat_relative = transform_matrix[:3, :3]
+            tmat_relative = transform_matrix[:3, 3:]
+
+            cv.drawFrameAxes(frame, mtx, dist, rmat_relative, tmat_relative, 0.1)
+
+        # Déplacement en y
+        if axis_y >= max_y and plus:
+            plus = False
+        if axis_y <= min_y and not plus:
+            plus = True
+        if plus:
+            axis_y += 0.01
+        else:
+            axis_y -= 0.01
+
+        # Déplacement en Z
+        if axis_z >= min_z and not plus_z:
+            plus_z = True
+        if axis_z <= max_z and plus_z:
+            plus_z = False
+
+        if plus_z:
+            axis_z -= 0.0025
+        else:
+            axis_z += 0.0025
+
+        cv.imshow('frame', frame)
+        if cv.waitKey(1) & 0xFF == ord('q'):
+            break
+
+        cap.release()
+        cv.destroyAllWindows()
--- calibration/calibration.py
+++ calibration/calibration.py
@@ -2,38 +2,44 @@ import numpy as np
 import cv2 as cv
 import glob
 
-objpoints = []
-imgpoints = []
 
-chessboardSize = (9, 6)
+def calibrate():
+    obj_points = []
+    img_points = []
+    
+    chessboard_size = (9, 6)
+    
+    criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+    
+    objp = np.zeros((6 * 9, 3), np.float32)
+    objp[:, :2] = np.mgrid[0:9, 0:6].T.reshape(-1, 2)
+    
+    images = glob.glob('*.jpg')
 
-criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+    for fname in images:
+        img = cv.imread(fname)
+        gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+        ret, corners = cv.findChessboardCorners(gray, chessboard_size, None)
+        print(ret)
+        if ret:
+            obj_points.append(objp)
+            corners2 = cv.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
+            img_points.append(corners2)
+            cv.drawChessboardCorners(img, chessboard_size, corners2, ret)
+            cv.imshow('img', img)
+            cv.waitKey(500)
+    
+    ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(obj_points, img_points, gray.shape[::-1], None, None)
+    mean_error = 0
+    for i in range(len(obj_points)):
+        imgpoints2, _ = cv.projectPoints(obj_points[i], rvecs[i], tvecs[i], mtx, dist)
+        error = cv.norm(img_points[i], imgpoints2, cv.NORM_L2)/len(imgpoints2)
+        mean_error += error
 
-objp = np.zeros((6 * 9, 3), np.float32)
-objp[:, :2] = np.mgrid[0:9, 0:6].T.reshape(-1, 2)
+    print("total error: {}".format(mean_error / len(obj_points)))
+    
+    np.savez('calibration.npz', mtx=mtx, dist=dist, rvecs=rvecs, tvecs=tvecs)
 
-images = glob.glob('*.jpg')
-framesize = (0, 0)
 
-for fname in images:
-    img = cv.imread(fname)
-    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-    ret, corners = cv.findChessboardCorners(gray, chessboardSize, None)
-    print(ret)
-    if ret:
-        objpoints.append(objp)
-        corners2 = cv.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
-        imgpoints.append(corners2)
-        cv.drawChessboardCorners(img, chessboardSize, corners2, ret)
-        cv.imshow('img', img)
-        cv.waitKey(500)
-
-ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
-mean_error = 0
-for i in range(len(objpoints)):
-    imgpoints2, _ = cv.projectPoints(objpoints[i], rvecs[i], tvecs[i], mtx, dist)
-    error = cv.norm(imgpoints[i], imgpoints2, cv.NORM_L2)/len(imgpoints2)
-    mean_error += error
-print( "total error: {}".format(mean_error/len(objpoints)) )
-
-np.savez('calibration.npz', mtx=mtx, dist=dist, rvecs=rvecs, tvecs=tvecs)
+if __name__ == "__main__":
+    calibrate()
